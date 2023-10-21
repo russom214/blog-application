8c709f8665ec06cbea0be5e7eaed775ddd034652
russom214 <russom214@gmail.com>
2023-10-21 15:38:17 -0400

    routed and linked components and added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Homepage from "./components/Homepage";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <div className="App">
-        <h1>My Blog</h1>
+        <NavBar />
+      </div>
+      <div id="page-body">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
     </>
   );
